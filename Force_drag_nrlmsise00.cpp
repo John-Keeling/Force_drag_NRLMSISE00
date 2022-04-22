2eb72b24db3e79269ddfd9eeff867e8849415e35
John Keeling <jpk_81@hotmail.co.uk>
2022-04-22 12:08:56 +0100

    nrlmsise00_density function broken down into seven smaller functions

--- Force_drag_nrlmsise00.cpp
+++ Force_drag_nrlmsise00.cpp
@@ -7,11 +7,13 @@
 #include "../include/Resident_space_object.h"
 #include "../include/Resident_variables.h"
 #include <unistd.h>
+#include <iostream>
 
 void Force_drag_nrlmsise00::setup(const Resident_constants &rso_const,
                               std::shared_ptr<Resident_variables> in_state) {
      Force_drag::setup(rso_const, in_state);
 }
+
 
 void Force_drag_nrlmsise00::compute_acceleration() {
     // Report error, this displays the issue to the user and halts simulation
@@ -24,28 +26,46 @@ void Force_drag_nrlmsise00::compute_acceleration() {
     }
 
     // USSA up to 1000 km, with exponential decay function above
-    double rho = nrlmsise00_density(state->geodetic.alt);
+    double rho = nrlmsise00_density(state->geodetic.alt, state->geodetic.lat,
+            state->geodetic.lon);
     state->atmos_density = rho;
     a_ecef = minus500C_dAm * rho * state->ecef_v * state->ecef_rso_vel;
     state->total_a_ecef += a_ecef;
 }
 
-double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
-    // Retrieves mass density from atmos. model for given time and location
-    
-    std::string model_latitude, model_longitude, model_altitude, mod_str1, 
-            mod_str2, mod_str3, mod_str4, str_dayyear, oneWord, break_str, 
-            stream1, stream, str_previous_day, str_f10year;
-    std::string model_year, model_month, model_day, model_hour, model_minute, 
-            model_second, r, s, t, u, v, w, x, y, z, search, line, 
-            line_stream1, str_convert;
-    std::stringstream model_output, model_utc1, model_utc2, line_stream;
 
-    //RETRIEVE LATITUDE, LONGITUDE AND ALTITUDE
+double Force_drag_nrlmsise00::nrlmsise00_density(double alt, double lat, 
+        double lon) {
+    // Retrieves mass density from atmos. model for given time and location
+
+    std::cout << "CHECK_1 \n";
+    auto [altitude, latitude, longitude] = msis_lla_coordinates(alt, lat, lon);
+    auto [day_of_year, previous_day, f10_year, second, day, month, year] 
+            = msis_time_stamp(state->eci.epoch.str_UTC_datestamp());
+    auto [F107_value, F107A_value] 
+            = msis_f107(previous_day, f10_year);
+    std::cout << "CHECK_00" << " " << year << " " << month << " " << day 
+            << "\n";
+    std::string Ap_value = ap_value(year, month, day);
+    double rho = retrieve_mass_density(day_of_year, year, second, altitude, 
+            latitude, longitude, F107_value, F107A_value, Ap_value);
+
+    return rho;
+
+};
+
+std::tuple<std::string, std::string, 
+        std::string> Force_drag_nrlmsise00::msis_lla_coordinates(double alt, 
+        double lat, double lon) {
+    // Retrieves latitude, longitude and altitude coordinates
+
+    std::cout << "CHECK_2 \n";
+    std::string model_latitude, model_longitude, model_altitude;
+    std::stringstream model_output;
+
     model_output << std::fixed << std::setprecision(15) << std::setw(18)
-                  << state->geodetic.lat << " " << std::setw(18)
-                  << state->geodetic.lon << " " << std::setw(18)
-                  << state->geodetic.alt << " " << std::setw(18);
+                  << lat << " " << std::setw(18) << lon << " " << std::setw(18)
+                  << alt << " " << std::setw(18);
     model_output >> model_latitude >> model_longitude >> model_altitude;
     if (model_latitude.length() > 8) {
         std::string str_new = model_latitude.substr (0, 8);
@@ -59,23 +79,28 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
         std::string str_new = model_altitude.substr (0, 8);
         model_altitude = str_new;
     }
-    float flt_alt = std::stof(model_altitude);
-    float flt_lat = std::stof(model_latitude);
-    float flt_lon = std::stof(model_longitude);
+    return {model_altitude, model_latitude, model_longitude};
+};
 
-    //CUMMULATIVE DAYS IN STANDARD / LEAP YEARS AND LIST OF LEAP YEARS
-    std::map<int, int> cal_days = {{1,0,}, {2,31,}, {3,59,}, {4,90,}, {5,120,}, 
-            {6,151,}, {7,181,}, {8,212,}, {9,243,}, {10,273,}, {11,304,}};
-    std::map<int, int> leap_days = {{1,0,}, {2,31,}, {3,60,}, {4,91,}, {5,121,}, 
-            {6,152,}, {7,182,}, {8,213,}, {9,244,}, {10,274,}, {11,305,}};
-    std::vector<int> leap_yrs = { 1992, 2996, 2000, 2004, 2008, 2012, 2016, 
-            2020 };
+
+std::tuple<std::string, std::string, std::string, std::string, std::string, 
+        std::string, std::string> Force_drag_nrlmsise00::msis_time_stamp(
+        std::string epoch) {
+    // Retrieves time stamp for MSIS model in specified format
+
+    std::cout << "CHECK_3 \n";
+    std::string mod_str1, mod_str2, mod_str3, mod_str4, oneWord, 
+        break_str, model_year, model_month, model_day, model_hour, 
+        model_minute, model_second;
+    std::stringstream model_utc1, model_utc2, stream_sec, stream_day, 
+        stream_month, stream_year;
+    int int_year, int_month, int_day;
     
-    //RETRIEVE UTC DATESTAMP & PARSE 
-    model_utc1 << state->eci.epoch.str_UTC_datestamp();
+    // Retrieves UTC timestamp and parses
+    model_utc1 << epoch;
     int count = 0;
     while(model_utc1 >> oneWord) { ++count;}
-    model_utc2 << state->eci.epoch.str_UTC_datestamp();
+    model_utc2 << epoch;
     if (count == 3) {
         model_utc2 >> break_str >> mod_str3 >> mod_str4;
         unsigned slash1 = break_str.find_first_of("/"); 
@@ -90,7 +115,6 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
     int len2 = mod_str2.length();
     int len3 = mod_str3.length();
     int len4 = mod_str4.length();   
-    int int_year, int_month, int_day, int_dayyear, int_previous_day, int_f10year;
     model_year = mod_str2.substr (len2 - 5, 4);
     int_year = std::stoi( model_year );
     if (len2 == 7) {
@@ -120,7 +144,48 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
             + (int_hour * 3600);
     model_second = std::to_string(int_sec);
 
-    //CHECK IF LEAP YEAR AND CALCULATE DAY OF YEAR
+    auto [dayyear, previous_day, f10year]
+            = leap_year_doy(int_year, int_month, int_day); 
+    
+    stream_sec << model_second;
+    stream_sec >> model_second;
+    stream_day << model_day;
+    stream_day >> model_day;
+    stream_month << model_month;
+    stream_month >> model_month;
+    stream_year << model_year;
+    stream_year >> model_year; 
+
+    std::cout << "CHECK_A" << " " << dayyear << " " << previous_day << " " 
+            << f10year << " " << model_second << " " << model_day << " " << 
+            model_month << " " << model_year << "\n";
+    
+    return {str_dayyear, str_previous_day, str_f10year, model_second, model_day, 
+            model_month, model_year};
+
+};
+
+std::tuple<std::string, std::string, 
+        std::string> Force_drag_nrlmsise00::leap_year_doy(int int_year, 
+        int int_month, int int_day) {
+    // Checks if leap year and returns year date and month
+
+    std::cout << "CHECK_4 " << std::to_string(int_year) << " " 
+            << std::to_string(int_month) << " " << std::to_string(int_day) 
+            << "\n" ;
+    //std::string str_dayyear, str_previous_day, str_f10year;
+    std::stringstream stream_dayyear, stream_previous_day, stream_f10year;
+    int int_dayyear, int_previous_day, int_f10year;
+
+    // Cummulative days in standard / leap years and list of leap years
+    std::map<int, int> cal_days = {{1,0,}, {2,31,}, {3,59,}, {4,90,}, {5,120,}, 
+            {6,151,}, {7,181,}, {8,212,}, {9,243,}, {10,273,}, {11,304,}};
+    std::map<int, int> leap_days = {{1,0,}, {2,31,}, {3,60,}, {4,91,}, {5,121,}, 
+            {6,152,}, {7,182,}, {8,213,}, {9,244,}, {10,274,}, {11,305,}};
+    std::vector<int> leap_yrs = { 1992, 2996, 2000, 2004, 2008, 2012, 2016, 
+            2020 };
+
+    // Checks if leap year and day of year
     std::map<int,int>::iterator it1;
     std::map<int,int>::iterator it2;
     if (std::find(leap_yrs.begin(), leap_yrs.end(), 
@@ -137,9 +202,9 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
                 int_f10year = int_year - 1;
                 int_previous_day = 365;
             }
-            str_dayyear = std::to_string(int_dayyear);
-            str_previous_day = std::to_string(int_previous_day);
-            str_f10year = std::to_string(int_f10year);
+            std::string str_dayyear = std::to_string(int_dayyear);
+            std::string str_previous_day = std::to_string(int_previous_day);
+            std::string str_f10year = std::to_string(int_f10year);
         }
         else {
             std::cout << "Month not found in cal_days.";
@@ -148,7 +213,7 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
     }
     else {
         // Look up month in leap_days then add the day of month to get 
-        //...day of year (int_dayyear) and previous day (int_previous_day)
+        // ...day of year (int_dayyear) and previous day (int_previous_day)
         it2 = leap_days.find(int_month);
         if (it2 != leap_days.end()) {
             int_dayyear = it2->second + int_day;
@@ -160,30 +225,52 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
                 int_f10year = int_year - 1;
                 int_previous_day = 366;
             }
-            str_dayyear = std::to_string(int_dayyear);
-            str_previous_day = std::to_string(int_previous_day);
-            str_f10year = std::to_string(int_f10year);
+            std::string str_dayyear = std::to_string(int_dayyear);
+            std::string str_previous_day = std::to_string(int_previous_day);
+            std::string str_f10year = std::to_string(int_f10year);
         }
         else {
             std::cout << "Month not found in leap_days.";
             abort();
         }
     }
+    
+    stream_dayyear << str_dayyear;
+    stream_dayyear >> str_dayyear;
+    stream_previous_day << str_previous_day;
+    stream_previous_day >> str_previous_day;
+    stream_f10year << str_f10year;
+    stream_f10year >> str_f10year; 
 
-    //RETRIEVE F10.7 & F10.7A SOLAR FLUX INDEX VALUES FROM FILE:
+    std::cout << "CHECK_B" << " " << str_dayyear << " " << str_previous_day 
+        << " " << str_f10year << "\n";
+
+    return {str_dayyear, str_previous_day, str_f10year};
+
+};
+
+
+std::tuple<std::string, std::string> Force_drag_nrlmsise00::msis_f107(
+        std::string prev_day, std::string f10_year) {
+    // Retrieves F10.7 & F10.7A solar flux index values from file:
+
+    std::cout << "CHECK_5 " << f10_year << "\n";
+    std::string F107_value, F107A_value, ignore1, ignore2, ignore3, search, 
+        line;
+    std::stringstream line_stream;
     std::ifstream inFileF107;
+
     inFileF107.open("/Users/johnkeeling/Desktop/Astrophysics_MSc/PHAS0062"
             "_research_project/hawke_files/msis-model_c/DATA/SOLFSMY.TXT");
-    std::string F107_value, F107A_value, w1, w2, w3;
 
-    if (str_previous_day.length() == 3) {
-        search = str_f10year + " " + str_previous_day; 
+    if (prev_day.length() == 3) {
+        search = f10_year + " " + prev_day; 
     }
-    else if (str_previous_day.length() == 2) {
-        search = str_f10year + "  " + str_previous_day; 
+    else if (prev_day.length() == 2) {
+        search = f10_year + "  " + prev_day; 
     }
     else {
-        search = str_f10year + "   " + str_previous_day;
+        search = f10_year + "   " + prev_day;
     }
     if(!inFileF107){
     std::cout << "Unable to open file" << std::endl;
@@ -195,18 +282,27 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
         pos=line.find(search);
         if(pos!=std::string::npos) {
                 line_stream << line;
-                line_stream >> w1 >> w2 >> w3 >> F107_value >> F107A_value;
+                line_stream >> ignore1 >> ignore2 >> ignore3 >> F107_value 
+                    >> F107A_value;
             }     
     }
+    return {F107_value, F107A_value};
+};
 
-    //RETRIEVE AP GEOMAGNETIC INDEX VALUES FROM FILE:
+
+std::string Force_drag_nrlmsise00::ap_value(std::string model_year, 
+        std::string model_month, 
+        std::string model_day) {
+    // Retrieve AP geomagnetic index value from file:
+
+    std::string Ap_value, str_ap_date, Ap1, Ap2, Ap3, Ap4, Ap5, 
+            Ap6, Ap7, Ap8, line_stream1, line;
     std::ifstream inFileAp;
+
     inFileAp.open("/Users/johnkeeling/Desktop/Astrophysics_MSc/"
             "PHAS0062_research_project/hawke_files/msis-model_c/DATA/apindex");
-    std::string Ap_value, str_ap_date, Ap1, Ap2, Ap3, Ap4, Ap5, 
-            Ap6, Ap7, Ap8;
 
-    //--CREATE SEARCH STRING (yymmdd):
+    // Create search string (yymmdd):
     if (model_month.length() == 1) {
         if(model_day.length() == 1) {
             str_ap_date = model_year.substr(2,2) + "0" 
@@ -233,8 +329,7 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
             }
         }
     }
-
-    //--FIND AVERAGE AP VALUE
+    // Find average AP value
     Ap1 = line_stream1.substr (0,3);
     Ap2 = line_stream1.substr (3,3);
     Ap3 = line_stream1.substr (6,3);
@@ -254,11 +349,23 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
     int int_Ap = round(float(int1 + int2 + int3 + int4 + int5 
             + int6 + int7 + int8) / 8);
     Ap_value = std::to_string(int_Ap);
+
+    return Ap_value;
+};
+
     
-    //COMMAND LINE INSTRUCTIONS TO RUN NRLMSISE00:
+double Force_drag_nrlmsise00::retrieve_mass_density(std::string str_dayyear, 
+        std::string model_year, std::string model_second, 
+        std::string model_altitude, std::string model_latitude, 
+        std::string model_longitude, std::string F107_value,
+        std::string F107A_value, std::string Ap_value) {
+    //Command line instructions to run NRLMSISE00:
+
+    std::cout << "CHECK_7 \n";
+    std::string str_start, str_end, str_res, str_convert;
+
     std::string cmd = std::string("/Users/johnkeeling/Desktop/Astrophysics_MSc/"
-            "PHAS0062_research_project/hawke_files/MSIS-model_c/"
-            "nrlmsise_test01") 
+            "PHAS0062_research_project/hawke_files/MSIS-model_c/nrlmsise_test01") 
             + std::string(" " + str_dayyear + " "  + model_year + " " 
             + model_second + "  " + model_altitude + "  " + model_latitude 
             + "  " + model_longitude + "  " + "0" + "  " + F107_value + "  " 
@@ -267,14 +374,12 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
             "PHAS0062_research_project/hawke_files/MSIS-model_c");  
     FILE *command=popen(cmd.c_str(), "r");
     char result[24]={0x0};
-    std::string str_res;
     while (fgets(result, sizeof(result), command) !=NULL)
     pclose(command);
     chdir("/Users/johnkeeling/Desktop/Astrophysics_MSc/"
             "PHAS0062_research_project/hawke_files/odl20lite/bin");
-    std::string str_start, str_end;
 
-    //REFORMAT DENSITY VALUE TO MAKE IT CPP COMPATIBLE
+    //Reformat density value to make it C++ compatible
     str_res = result;
     double rho;
     if (str_res == "inf" || str_res.substr(8,1) != "e") {
@@ -292,6 +397,5 @@ double Force_drag_nrlmsise00::nrlmsise00_density(double alt) {
         str_res = str_start + "E" + str_convert;
         rho = std::stod(str_res); 
     }
-    
     return rho;
 };
